d698edfecc45954fc7e171fa64e54b22985e7025
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2015-10-06 10:31:57 +0200

    Code for latest page

--- controllers/home.php
+++ controllers/home.php
@@ -10,7 +10,7 @@ class Home extends Controller {
       $data['title'] = 'Home';
       $data['new_releases'] = $this->_model->all();
       $data['datum'] = date("F j,Y, g:i a");
-      
+
       $this->connect();
       $this->_view->render('header', $data);
       $this->_view->render('home', $data);
@@ -49,7 +49,7 @@ class Home extends Controller {
   	{
 	      //Read a web page and check for errors:
 	      $url = "http://ger.whatsnewonnetflix.com/";
-	       
+
 	      $result = $this->get_web_page( $url );
 
 	      if ( $result['errno'] != 0 )
@@ -60,9 +60,9 @@ class Home extends Controller {
 
 	      $page = $result['content'];
 
-	      if($result==TRUE){  
+	      if($result==TRUE){
 	          //explode
-	          $str = $page; 
+	          $str = $page;
 	          //title
 	          $preg_title=preg_match_all('/<h1 id="page_title">(.*)<\/h1>/iU', $str, $title);
 	          if($preg_title==TRUE){
@@ -74,33 +74,33 @@ class Home extends Controller {
 	          }
 	          $preg_movie_title=preg_match_all('/<h4><a.*?>(.*?)<\/a><\/h4>/iU', $str, $movie_title);
 	          $preg_img=preg_match_all('/(?<!_)src=\"(.*?)(.jpg)"/', $str, $image);
-			  $preg_description=preg_match_all('/<p>(.*?)<\/p>/s', $str, $description);
+			      $preg_description=preg_match_all('/<p>(.*?)<\/p>/s', $str, $description);
 	          $preg_movie_year=preg_match_all('/<span class="year">(.*?)<\/span>/s', $str, $movie_year);
 	          $preg_genres=preg_match_all('/<p class="genres">(.*?)<\/p>/s', $str, $genres);
 	          //check all preg match
 	          if($preg_movie_title==TRUE&&$preg_img==TRUE&&$preg_description==TRUE&&$preg_movie_year==TRUE&&$preg_genres==TRUE){
-	          	for ($i=count($movie_title[1]); $i >0; $i--) { 
-	          		//echo  $description[1][$i+2];;
+	          	for ($i=count($movie_title[1]); $i >0; $i--) {
+                //echo $description[1][$i+3];
 	          		//serialize content
 	          		$data['movie_title'] = $movie_title[1][$i-1];
-			        $data['movie_image'] = $image[1][$i-1];
-			        $data['movie_description'] = $description[1][$i+2];
-			        $data['movie_genres'] = $genres[1][$i-1];
-			        $data['movie_year'] = $movie_year[1][$i-1];
-			        //check if title already exists
-	          		$datac= $movie_title[1][$i-1];
-				    $how['numrows'] = $this->_model->check($datac);
-				    foreach ($how['numrows'] as $sum){
-				    	//if the title exists, no aaction
-				    	if($sum['num_rows'] > 0)
-					    {
-					    	//No Action
-					    }else{
-					    	//insert to DB
-			          		$this->_model->insert($data);
-					    }
-				    } // end of foreach ($how['numrows'] as $sum){
-	       		} // end of for ($i=count($movie_title[1]); $i >0; $i--) { 
+  			        $data['movie_image'] = $image[1][$i];
+  			        $data['movie_description'] = $description[1][$i+3];
+  			        $data['movie_genres'] = $genres[1][$i-1];
+  			        $data['movie_year'] = $movie_year[1][$i-1];
+  			        //check if title already exists
+  	          	$datac= $movie_title[1][$i-1];
+    				    $how['numrows'] = $this->_model->check($datac);
+    				    foreach ($how['numrows'] as $sum){
+    				    	//if the title exists, no action
+    				    	if($sum['num_rows'] > 0)
+    					    {
+    					    	//No Action
+    					    }else{
+    					    	//insert to DB
+    			          		$this->_model->insert($data);
+    					    }
+  				      } // end of foreach ($how['numrows'] as $sum){
+	       		 } // end of for ($i=count($movie_title[1]); $i >0; $i--) {
 	        } // end of check all preg match
 		} // end of if($result==TRUE){
 	}
